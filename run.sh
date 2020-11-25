c26cdceaf282a76ce4e5cbb3784d3ab1764a6063
jinshi <wangjinshi1992@gmail.com>
2020-11-25 06:05:08 +0100

    third_upload

---
+++ run.sh
@@ -0,0 +1,13 @@
+!python run_classifier.py \
+  --task_name=NER \
+  --do_train=true \
+  --do_eval=true \
+  --data_dir=data \
+  --vocab_file=vocab.txt \
+  --bert_config_file=bert_config.json \
+  --init_checkpoint=bert_model.ckpt \ 
+  --train_batch_size=32 \
+  --learning_rate=2e-5 \
+  --num_train_epochs=3.0 \
+  --max_seq_length=128 \
+  --output_dir=output \
